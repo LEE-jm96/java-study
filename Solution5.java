26cf838497c1dcfccc08becf1244aaf75657f82a
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2022-01-28 22:43:31 +0900

    ljm-05

--- Solution5.java
+++ Solution5.java
@@ -9,24 +9,24 @@ package codingTest;
 public class Solution5 {
 	public static int solution(int num) {
 		long ac = num;
-	    int answer = 0;
-	    while(ac != 1){
-	    	if(ac % 2 == 0){
-	    		ac = (ac / 2);
-	        }
-	        else {
-	            ac = (ac * 3) + 1; 
-	        }
-	        answer++;
-	        if(answer == 500){ 
-	            answer = -1; 
-	            break; 
-	        }
-	    }
-	    return answer;
-	    }
+	    	int answer = 0;
+	    	while(ac != 1){
+	    		if(ac % 2 == 0){
+	    			ac = (ac / 2);
+	        	}
+	        	else {
+	            		ac = (ac * 3) + 1; 
+	        	}
+	        	answer++;
+	        	if(answer == 500){ 
+	            		answer = -1; 
+	            		break; 
+	        	}
+	    	}
+	    	return answer;
+	}
 	
-	    public static void main(String[] args) {
-	    	System.out.println(solution(626331));
-	    }
+	public static void main(String[] args) {
+		System.out.println(solution(626331));
+	}
 }
