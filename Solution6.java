db7eeeddf7367f2c2a7d5b99fac8b7f84a88a76a
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2022-01-28 22:16:51 +0900

    ljm-06

--- Solution6.java
+++ Solution6.java
@@ -1,3 +1,8 @@
+/* 코딩 테스트 공부 - 6
+ * 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다.
+ *  numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+ */
+
 package codingTest;
 
 public class Solution6 {
