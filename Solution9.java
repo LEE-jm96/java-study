25b07e61f3a538f5ae30af18877b57afcd24a0e0
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2022-02-01 19:04:25 +0900

    ljm-09

--- Solution9.java
+++ Solution9.java
@@ -1,3 +1,7 @@
+/* 코딩 테스트 공부 - 9
+ * 정수 배열 numbers가 주어집니다. 
+ * numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+ */
 package codingTest;
 
 import java.util.List;
@@ -27,8 +31,8 @@ public class Solution9 {
         return answer;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
     	int[] number = {5, 0, 2, 7};
 		System.out.println(Arrays.toString(solution(number)));
-	}
+    }
 }
