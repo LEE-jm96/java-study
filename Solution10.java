0538f566468fb4f86cfb9c14b3f0d9b910591c37
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2022-02-02 12:10:23 +0900

    ljm-10

--- Solution10.java
+++ Solution10.java
@@ -1,3 +1,7 @@
+/* 코딩 테스트 공부 - 9
+ * 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다.
+ * s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+ */
 package codingTest;
 
 public class Solution10 {
@@ -15,7 +19,7 @@ public class Solution10 {
     public static void main(String[] args) {
 		String arr = "23four5six78nine";
 		System.out.println(solution(arr));
-	}
+    }
 }
 
 
