dafca335575089e6e330475da98bd23644ab4e01
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2022-02-03 20:00:18 +0900

    ljm-11

--- Solution11.java
+++ Solution11.java
@@ -1,3 +1,11 @@
+/* 코딩 테스트 공부 - 11
+ * 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+ * 예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+ * array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+ * 1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+ * 2에서 나온 배열의 3번째 숫자는 5입니다.
+ */
+
 package codingTest;
 
 import java.util.Arrays;
@@ -18,9 +26,9 @@ public class Solution11 {
         return answer;
     }
 	
-	public static void main(String[] args) {
-		int[] arr = {1, 5, 2, 6, 3, 7, 4};
-		int[][] commands = { {2, 5, 3}, {4, 4, 1}, {1, 7, 3} };
-		System.out.println(solution(arr, commands));
-	}
+    public static void main(String[] args) {
+	    int[] arr = {1, 5, 2, 6, 3, 7, 4};
+	    int[][] commands = { {2, 5, 3}, {4, 4, 1}, {1, 7, 3} };
+	    System.out.println(solution(arr, commands));
+    }
 }
